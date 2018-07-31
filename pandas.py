11720d19ae81d492c302ae780aed0dc8f60c6757
ggg <40929360+w8q@users.noreply.github.com>
2018-07-31 08:10:20 +0800

    added qqplot

--- pandas.py
+++ pandas.py
@@ -1,11 +1,12 @@
 import gc
 from collections import OrderedDict as odict
+from itertools import count
 from typing import List, Tuple, Callable
 
 import numpy as np
+import scipy as sp
 import pandas as pd
 
-from IPython.display import display
 
 
 def csv2df(path, **kw):
@@ -13,6 +14,14 @@ def csv2df(path, **kw):
     df.info()
     print('shape: {}'.format(df.shape))
     return df
+
+
+
+palette_dtypes = {np.dtype('object'): '#fd7272',
+                  np.dtype('float32'): '#00b894',
+                  np.dtype('float64'): '#60a3bc',
+                  np.dtype('int64'): '#ff9f1a',
+                  np.dtype('int32'): '#fed330'}
 
 
 def frame_info(frame: pd.DataFrame,
@@ -58,4 +67,33 @@ def frame_info(frame: pd.DataFrame,
                .applymap(bg_dtypes, subset=['dtypes'])
                .applymap(fg_frac_nan, subset=['frac_nan'])
                .applymap(fg_frac_unique, subset=['frac_unique']))
+
+
+
+def qqplot(frame: pd.DataFrame,
+           dtypes_include: Tuple[str]=('int32', 'int64', 'float32', 'float64'),
+           dtypes_exclude: Tuple[str]=('object',)):
+
+    # include/exclude columns by dtypes
+    columns = frame.select_dtypes(include=dtypes_include, exclude=dtypes_exclude).columns
+    # how many columns will be plotted
+    ncol = len(columns)
+
+    # setup figure size, number of rows/cols
+    fig_cols = 5
+    fig_rows = (ncol % fig_cols != 0) + ncol // fig_cols
+    fig_size = (19, 3.6 * fig_rows)
+    fig, ax = plt.subplots(fig_rows, fig_cols, figsize=fig_size)
+
+    # fill qqplots into figure
+    g = ((p, q) for p in count(0) for q in range(fig_cols))
+    for (r, c), column in zip(g, columns):
+        # https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.probplot.html
+        (x, y), (m, b, _) = sp.stats.probplot(frame[column].dropna())
+        color = palette_dtypes.get(frame[column].dtype, '#9b59b6')
+        ax[r][c].plot(x, y, color=color, marker='|', linestyle='')
+        ax[r][c].plot(x, m*x+b, color='#4b4b4b', linestyle='-', linewidth=.9)
+        ax[r][c].set_title(column, fontsize=10)
+
+    gc.collect()
 
