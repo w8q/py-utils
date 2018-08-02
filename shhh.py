b42632dc3624b240ea12151fe744b2747eb50c7e
ggg <40929360+w8q@users.noreply.github.com>
2018-08-02 13:43:49 +0800

    uppercased context name

--- shhh.py
+++ shhh.py
@@ -4,7 +4,7 @@ from contextlib import contextmanager
 
 # https://stackoverflow.com/a/13250224
 @contextmanager
-def shhh(out=None, err=None):
+def Shhh(out=None, err=None):
     '''Redirect std{out,err}'''
     saved = sys.stdout, sys.stderr
     sys.stdout, sys.stderr = out, err
