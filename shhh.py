1c3d27c74a36d16ec9d4b4ddfe320319c86b428f
ggg <40929360+w8q@users.noreply.github.com>
2018-08-02 11:00:22 +0800

    added shhh.py

---
+++ shhh.py
@@ -0,0 +1,14 @@
+import sys
+from contextlib import contextmanager
+
+
+# https://stackoverflow.com/a/13250224
+@contextmanager
+def shhh(out=None, err=None):
+    '''Redirect std{out,err}'''
+    saved = sys.stdout, sys.stderr
+    sys.stdout, sys.stderr = out, err
+    try:
+        yield
+    finally:
+        sys.stdout, sys.stderr = saved
