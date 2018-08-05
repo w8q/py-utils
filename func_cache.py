8b867d5942f189bd6cd14a764ad7ec1694b1c83f
ggg <40929360+w8q@users.noreply.github.com>
2018-08-05 22:37:04 +0800

    added func_cache.py

---
+++ func_cache.py
@@ -0,0 +1,60 @@
+from functools import wraps, partial
+from pathlib import Path
+import pickle
+import joblib
+import hashlib
+
+from timer import Timer
+
+               
+def hash_args(obj, h=None):
+    h = h or hashlib.sha256()
+    if isinstance(obj, dict):
+        for k, v in sorted(obj.items()):
+            h.update(pickle.dumps(k))
+            hash_args(v, h)
+    elif isinstance(obj, (list, tuple, set)):
+        for e in obj:
+            hash_args(e, h)
+    else:
+        h.update(pickle.dumps(obj))
+    return h.hexdigest()
+
+
+
+def func_cache(f, cache_dir='./cache', return_path=False):
+
+    @wraps(f)
+    def wrapper(*args, **kw):
+        
+        # name of wrapped func
+        func_name = f.__name__
+
+        # compute checksum (filename)
+        with Timer(f'<{func_name}> Compute checksum', LOG.info):
+            h = hashlib.sha256()
+            hash_args(args, h)
+            hash_args(kw, h)
+            checksum = h.hexdigest()
+
+        # make dir/path
+        dirpath = f'{cache_dir}/FUNC.{func_name}'
+        Path(dirpath).mkdir(exist_ok=True)
+        filepath = f'{dirpath}/{checksum}.pkl'
+        
+        # check if cache exists
+        if Path(filepath).is_file():
+            with Timer(f'<{func_name}> Load result from cache ({filepath})', LOG.info):
+                return filepath if return_path else joblib.load(filepath)
+        
+        # compute value, and save result as feather format
+        with Timer(f'<{func_name}> Compute result', LOG.info):
+            val = f(*args, **kw)
+        with Timer(f'<{func_name}> Save result to cache ({filepath})', LOG.info):
+            joblib.dump(val, filepath)
+        if return_path:
+            del val
+            return filepath
+        return val
+
+    return wrapper
